327c458b684a4cb2aeb073935dc64e88e483ffb7
ant0177 <ant017@ucsd.edu>
2023-11-30 22:17:53 -0800

    website textbox

--- Microsoft-Hackathon/client/src/App.jsx
+++ Microsoft-Hackathon/client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hackathon</h1>
+      <h1>College Major Quiz</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
